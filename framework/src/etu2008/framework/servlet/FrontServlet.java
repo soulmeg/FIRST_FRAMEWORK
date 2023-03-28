8daaea83b3c29a8686b8b5544436a20aad21cdf3
meg <rakotonarivomegane@gmail.com>
2023-03-28 05:58:42 +0200

    change directory 1

--- framework/src/etu2008/framework/servlet/FrontServlet.java
+++ framework/src/etu2008/framework/servlet/FrontServlet.java
@@ -5,24 +5,22 @@
 package etu2008.framework.servlet;
 
 import etu2008.framework.Mapping;
-import etu2008.framework.annotations.url;
-import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
-import java.net.URL;
-import java.lang.reflect.Method;
+
 /**
  *
  * @author megane
  */
+//@WebServlet(name = "Front", urlPatterns = {"/Front"})
 public class FrontServlet extends HttpServlet {
-    HashMap<String, Mapping> mappingUrls;
+    HashMap<String, Mapping> mapi=new HashMap<>();
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -32,47 +30,30 @@ public class FrontServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-     @Override
-    public void init() throws ServletException {
-        try {
-            mappingUrls = new HashMap<String, Mapping>();
-            String packageName = "etu2008.framework.dataObject";
-            URL f = Thread.currentThread().getContextClassLoader().getResource(packageName.replace(".", "/"));
-            for (File file : new File(f.getFile()).listFiles()) {
-                if (file.getName().contains(".class")) {
-                    String className = file.getName().replaceAll(".class$", "");
-                    Class<?> cls = Class.forName(packageName + "." + className);
-                    for (Method method : cls.getDeclaredMethods()) {
-                        if (method.isAnnotationPresent(url.class)) {
-                            mappingUrls.put(method.getAnnotation(url.class).value(), new Mapping(cls.getName(), method.getName()));
-                        }
-                    }
-                }
-            }
-        } catch (Exception e) {
-            throw new ServletException(e);
-        }
+    public void init(){
+        
     }
-
-
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-       try (PrintWriter out = response.getWriter()) {
+        try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-           
+            Mapping p=new Mapping("classe","get");
+            this.mapi.put("test",p);
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet FrontServlet </title>");            
+            out.println("</head>");
+            out.println("<body>");
             out.println("<h1>Servlet FrontServlet at " + request.getContextPath() + "</h1>");
             out.println(request.getServletPath());
-     
-             if(request.getParameter("anarana") != null){
-                  out.println("<p> Hello "+request.getParameter("anarana")+" !! </p>");
-            } 
-             
-             for (Map.Entry<String, Mapping> entry : mappingUrls.entrySet()) {
-                out.println("Nom de l'url: "+entry.getKey()+" //Nom de la classe: "+ entry.getValue().getClassName()+" //Nom des methodes: "+ entry.getValue().getMethod());
-            
-             }
-           
+            out.println("atooo "+p.getClassName());
+            if(request.getParameter("anarana") != null){
+                  out.println("<p>Welcome "+request.getParameter("anarana")+" !! </p>");
+            }   
+            out.println("</body>");
+            out.println("</html>");
         }
     }
 
