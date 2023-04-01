56de405ed8fa1d26deda4d55a2acd9636dc020e5
meg <rakotonarivomegane@gmail.com>
2023-04-01 15:12:26 +0200

    final sprint4 2.0

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 cd
-javac -d /home/megane/classes /home/megane/Github/FIRST_FRAMEWORK/framework/*.java
-sudo cp -r /home/megane/classes/dataObject /home/megane/Github/FIRST_FRAMEWORK/test_framework/WEB-INF/classes/
-jar cf framework.jar -C classes/ .
-sudo mv framework.jar /home/megane/Github/FIRST_FRAMEWORK/test_framework/WEB-INF/lib
+javac -d /home/megane/classes /home/megane/GitHub/FIRST_FRAMEWORK/framework/*.java
+sudo cp -r /home/megane/classes/dataObject /home/megane/GitHub/FIRST_FRAMEWORK/test_framework/WEB-INF/classes/
+jar cf framework.jar -C /home/megane/classes/ .
+sudo mv framework.jar /home/megane/GitHub/FIRST_FRAMEWORK/test_framework/WEB-INF/lib
 #cp -r /home/megane/FRAMEWORK/test_framework /home/megane/apache-tomcat-8.5.81/webapps/
-jar cf launch_framework.war -C /home/Github/FIRST_FRAMEWORK/test_framework/ . 
+jar cf launch_framework.war -C /home/megane/GitHub/FIRST_FRAMEWORK/test_framework/ . 
 sudo mv /home/megane/launch_framework.war /home/megane/apache-tomcat-8.5.81/webapps/
 #cp -r launch_framework.war 
 sudo sh /home/megane/apache-tomcat-8.5.81/bin/shutdown.sh
